2a267e9685149ec20e11c69cb619e9a0f8403ead
Hitesh <hiteshspatil99@gmail.com>
2021-11-15 00:14:40 +0530

    [Hitesh] add : Calculate Daily Employee Wage

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -6,27 +6,19 @@ namespace EmployeeWageComputation
 {
     class EmployeeWageCalculation
     {
-        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, PRESENT = 1, HALF_DAY = 2, TOTAL_DAYS = 20, TOTAL_HOUR = 100;
-        int totalSalary, empHr;
-        public void MonthlyEmployeeWage()
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8;
+        int totalSalary;
+        public void EmployeeDailyWag()
         {
             Random rand = new Random();
-            int empCheck = rand.Next(0, 3);
-            for (int i = 0; i < TOTAL_DAYS && i < TOTAL_HOUR; i++)
-                switch (empCheck)
-                {
-                    case PRESENT:
-                        this.empHr += FULL_DAY_HOUR;
-                        break;
-                    case HALF_DAY:
-                        this.empHr += HALF_DAY_HOUR;
-                        break;
-                    default:
-                        Console.WriteLine("Employee is Absent for the Day");
-                        break;
-                }
-            this.totalSalary = WAGE_PR_HR * empHr;
-            Console.WriteLine("Here is the Monthly Salary of Emp." + this.totalSalary);
+            int empCheck = rand.Next(0, 2);
+            if (empCheck == 1)
+            {
+                this.totalSalary = WAGE_PR_HR * FULL_DAY_HOUR;
+                Console.WriteLine("Employee is Present and Salary is: " + this.totalSalary);
+            }
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
