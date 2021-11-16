d4809377801286e14f92ebb1b3af0bed6fe77f87
Hitesh <hiteshspatil99@gmail.com>
2021-11-16 23:04:43 +0530

    [Hitesh] add : Limited Working Hrs Or Days

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -6,24 +6,24 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWageCalculation
     {
-        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8, PART_WORKING_HRS = 4, WORKING_DAYS = 20;
-        int totalEmpWage, empHrs;
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGE_PER_HR = 20, FULL_WORKING_HRS = 8, PART_WORKING_HRS = 4, WORKING_DAYS = 20, MAX_WORKING_HRS = 100;
+        int totalEmpWage, empHrs = 0;
         public void MonthlyEmployeeWage()
         {
             Random random = new Random();
-            for (int i = 0; i < WORKING_DAYS; i++)
+            for (int i = 0; i < WORKING_DAYS && empHrs < MAX_WORKING_HRS; i++)
             {
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
                     case IS_FULL_TIME:
-                        this.empHrs = FULL_WORKING_HRS;
+                        this.empHrs += FULL_WORKING_HRS;
                         break;
                     case IS_PART_TIME:
-                        this.empHrs = PART_WORKING_HRS;
+                        this.empHrs += PART_WORKING_HRS;
                         break;
                     default:
-                        this.empHrs = 0;
+                        this.empHrs += 0;
                         break;
                 }
             }
