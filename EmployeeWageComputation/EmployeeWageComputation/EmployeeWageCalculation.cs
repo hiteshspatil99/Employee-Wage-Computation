ad700d3e6b0c7c5e51cde90e132ac8d353e2e018
Hitesh <hiteshspatil99@gmail.com>
2021-11-16 22:51:38 +0530

    [Hitesh] add : Using Switch Case For EmpWage

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -6,34 +6,25 @@ namespace EmployeeWageComputation
 {
     class EmployeeWageCalculation
     {
-        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, PRESENT = 1, HALF_DAY = 2, TOTAL_DAYS = 20, TOTAL_HOUR = 100;
-        int totalSalary, empHr, totalEmpHrs = 0, totalWorkingDays = 0;
-        public int ComputeEmployeeWage(string company, int empRatePrHr, int numOfWorkingDays, int maxHourPrMonth)
+        public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, PRESENT = 1, HALF_DAY = 2;
+        int totalSalary, TOTAL_HOUR = 0;
+        public void MonthlyEmployeeWage()
         {
             Random rand = new Random();
             int empCheck = rand.Next(0, 3);
-            while (totalEmpHrs <= maxHourPrMonth && totalWorkingDays <= numOfWorkingDays)
+            switch (empCheck)
             {
-                totalWorkingDays++;
-                switch (empCheck)
-                {
-                    case PRESENT:
-                        this.empHr += FULL_DAY_HOUR;
-                        break;
-                    case HALF_DAY:
-                        this.empHr += HALF_DAY_HOUR;
-                        break;
-                    default:
-                        Console.WriteLine("Employee is Absent for the Day");
-                        break;
-                }
-                totalEmpHrs += empHr;
-                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs : " + empHr);
-
+                case PRESENT:
+                    this.TOTAL_HOUR = FULL_DAY_HOUR;
+                    break;
+                case HALF_DAY:
+                    this.TOTAL_HOUR = HALF_DAY_HOUR;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent for the Day");
+                    break;
+                    this.totalSalary = WAGE_PR_HR * TOTAL_HOUR;
+                    Console.WriteLine("Employee Salary is: " + totalSalary);
             }
-            this.totalSalary = WAGE_PR_HR * totalEmpHrs;
-            Console.WriteLine("Here is the Monthly Wage of Emp." + this.totalSalary);
-            return totalSalary;
-        }
     }
 }
--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -7,17 +7,8 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter the Company Name: ");
-            string company = Console.ReadLine();
-            Console.WriteLine("Enter the Rate per Hour: ");
-            int empRatePrHr = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Number of Working Days: ");
-            int numOfWorkingDays = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Number of Max Hour Per Month: ");
-            int maxHourPrMonth = Convert.ToInt32(Console.ReadLine());
-
-            EmployeeWageCalculation empWage = new EmployeeWageCalculation();
-            empWage.ComputeEmployeeWage(company, empRatePrHr, numOfWorkingDays, maxHourPrMonth);
+            EmployeeWageCalculation empAtt = new EmployeeWageCalculation();
+            empAtt.MonthlyEmployeeWage();
         }
     }
 }
