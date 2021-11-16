a9d7f8411e1989f9b4ef025306ccf03f2db10b96
Hitesh <hiteshspatil99@gmail.com>
2021-11-16 22:24:51 +0530

    [Hitesh] add : Emp Wage For Multiple Companies

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWageCalculation.cs
@@ -8,11 +8,11 @@ namespace EmployeeWageComputation
     {
         public const int WAGE_PR_HR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, PRESENT = 1, HALF_DAY = 2, TOTAL_DAYS = 20, TOTAL_HOUR = 100;
         int totalSalary, empHr, totalEmpHrs = 0, totalWorkingDays = 0;
-        public void MonthlyEmployeeWage()
+        public int ComputeEmployeeWage(string company, int empRatePrHr, int numOfWorkingDays, int maxHourPrMonth)
         {
             Random rand = new Random();
             int empCheck = rand.Next(0, 3);
-            while (totalEmpHrs <= TOTAL_HOUR && totalWorkingDays <= TOTAL_DAYS)
+            while (totalEmpHrs <= maxHourPrMonth && totalWorkingDays <= numOfWorkingDays)
             {
                 totalWorkingDays++;
                 switch (empCheck)
@@ -33,7 +33,7 @@ namespace EmployeeWageComputation
             }
             this.totalSalary = WAGE_PR_HR * totalEmpHrs;
             Console.WriteLine("Here is the Monthly Wage of Emp." + this.totalSalary);
-
+            return totalSalary;
         }
     }
 }
--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -6,8 +6,18 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            EmployeeWageCalculation empAtt = new EmployeeWageCalculation();
-            empAtt.MonthlyEmployeeWage();
+
+            Console.WriteLine("Enter the Company Name: ");
+            string company = Console.ReadLine();
+            Console.WriteLine("Enter the Rate per Hour: ");
+            int empRatePrHr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number of Working Days: ");
+            int numOfWorkingDays = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number of Max Hour Per Month: ");
+            int maxHourPrMonth = Convert.ToInt32(Console.ReadLine());
+
+            EmployeeWageCalculation empWage = new EmployeeWageCalculation();
+            empWage.ComputeEmployeeWage(company, empRatePrHr, numOfWorkingDays, maxHourPrMonth);
         }
     }
 }
